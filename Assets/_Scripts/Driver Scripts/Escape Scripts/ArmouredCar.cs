cff027e43a2cc7448d6418b76beaaf02d4193b60
Danny-HypnoTank <d.pymhember@aol.co.uk>
2021-06-17 15:49:34 +0100

    Added explosion, start screens, win and lose states for escape and collection

--- Assets/_Scripts/Driver Scripts/Escape Scripts/ArmouredCar.cs
+++ Assets/_Scripts/Driver Scripts/Escape Scripts/ArmouredCar.cs
@@ -6,6 +6,11 @@ public class ArmouredCar : MonoBehaviour
 {
     [SerializeField]
     private int Health;
+    [SerializeField]
+    private GameObject moneyOBJ;
+    private int moneySpawnNum = 1;
+    [SerializeField]
+    private GameObject explosion;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,6 +19,16 @@ public class ArmouredCar : MonoBehaviour
             Health--;
             if (Health <= 0)
             {
+                for (int i = 0; i < moneySpawnNum; i++)
+                {
+                    Vector3 spawnPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate(moneyOBJ, spawnPosition, spawnRotation);
+                    if (explosion != null)
+                    {
+                        Instantiate(explosion, transform.position, transform.rotation);
+                    }
+                }
                 Destroy(this.gameObject);
             }
         }
